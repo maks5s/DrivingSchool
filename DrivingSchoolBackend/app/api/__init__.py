426ff7e91e21bee7ef5db1e5bdd97aead2a91735
maks5s <mskrypnyk0@gmail.com>
2025-04-25 00:35:22 +0300

    Moved router prefix

--- DrivingSchoolBackend/app/api/__init__.py
+++ DrivingSchoolBackend/app/api/__init__.py
@@ -1,3 +1,5 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+
+router = APIRouter(prefix=settings.api.prefix)
--- DrivingSchoolBackend/app/main.py
+++ DrivingSchoolBackend/app/main.py
@@ -19,7 +19,7 @@ async def lifespan(app: FastAPI):
 main_app = FastAPI(
     lifespan=lifespan,
 )
-main_app.include_router(api_router, prefix=settings.api.prefix)
+main_app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:main_app", host=settings.run.host, port=settings.run.port, reload=True)
