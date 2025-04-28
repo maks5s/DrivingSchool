d879d006788748622ff3ea9da5525671818f2afe
maks5s <mskrypnyk0@gmail.com>
2025-04-28 13:20:26 +0300

    Created functions to hash and validate passwords

--- DrivingSchoolBackend/app/auth/utils.py
+++ DrivingSchoolBackend/app/auth/utils.py
@@ -1,3 +1,4 @@
+import bcrypt
 import jwt
 
 from core.config import settings
@@ -27,3 +28,22 @@ def decode_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+def hash_password(
+    password: str,
+) -> str:
+    salt = bcrypt.gensalt()
+    pwd_bytes = password.encode()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode()
+
+
+def validate_password(
+    password: str,
+    hashed_password: str,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password.encode()
+    )
