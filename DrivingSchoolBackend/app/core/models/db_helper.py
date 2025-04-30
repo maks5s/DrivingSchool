84a3ef68862ad96fb7f386bd8f4617adb9622a1c
maks5s <mskrypnyk0@gmail.com>
2025-04-30 23:57:52 +0300

    Added database connection string parameters to DatabaseHelper

--- DrivingSchoolBackend/app/core/models/db_helper.py
+++ DrivingSchoolBackend/app/core/models/db_helper.py
@@ -7,11 +7,24 @@ class DatabaseHelper:
     def __init__(
         self,
         url: str,
+        host: str,
+        port: int,
+        db_name: str,
+        dbms_engine: str,
         echo: bool = False,
         echo_pool: bool = False,
         pool_size: int = 5,
         max_overflow: int = 10,
     ):
+        self.host: str = host
+        self.port: int = port
+        self.db_name: str = db_name
+        self.dbms_engine: str = dbms_engine
+        self.echo: bool = echo
+        self.echo_pool: bool = echo_pool
+        self.pool_size: int = pool_size
+        self.max_overflow: int = max_overflow
+
         self.engine = create_async_engine(
             url=url,
             echo=echo,
@@ -36,6 +49,10 @@ class DatabaseHelper:
 
 db_helper = DatabaseHelper(
     url=str(settings.db.url),
+    host=settings.db.host,
+    port=settings.db.port,
+    db_name=settings.db.db_name,
+    dbms_engine=settings.db.dbms_engine,
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     pool_size=settings.db.pool_size,
