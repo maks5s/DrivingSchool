a98f9f7139ea488bda3007776e13130d409728a9
maks5s <mskrypnyk0@gmail.com>
2025-05-01 00:03:17 +0300

    Created auth endpoint

--- DrivingSchoolBackend/app/api/__init__.py
+++ DrivingSchoolBackend/app/api/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .auth import router as auth_router
 
 router = APIRouter(prefix=settings.api.prefix)
+router.include_router(auth_router)
---
+++ DrivingSchoolBackend/app/api/auth.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+from core.schemas.login import LoginSchema
+from core.schemas.token import TokenInfo
+from auth import user as auth_user
+from auth import utils as auth_utils
+
+router = APIRouter(prefix="auth", tags=["Authentication"])
+
+
+@router.post("/login", response_model=TokenInfo)
+async def auth_user_issue_jwt(
+    login_data: LoginSchema,
+):
+    user = await auth_user.validate_auth_user(login_data.username, login_data.password)
+    jwt_payload = {
+        "sub": str(user.id),
+        "username": user.username,
+        "password": user.password,
+        "role": user.role,
+    }
+    token = auth_utils.encode_jwt(jwt_payload)
+    return TokenInfo(
+        access_token=token,
+        token_type="Bearer",
+    )
