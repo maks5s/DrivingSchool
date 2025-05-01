9341102eb5c9291eccce5222666b800463fcf422
maks5s <mskrypnyk0@gmail.com>
2025-05-01 15:00:10 +0300

    Added category level endpoints

--- DrivingSchoolBackend/app/api/__init__.py
+++ DrivingSchoolBackend/app/api/__init__.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 from core.config import settings
 from .auth import router as auth_router
+from .category_level import router as category_level_router
 
 router = APIRouter(prefix=settings.api.prefix)
 router.include_router(auth_router)
+router.include_router(category_level_router)
---
+++ DrivingSchoolBackend/app/api/category_level.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from core.schemas.category_level import CategoryLevelReadSchema, CategoryLevelCreateSchema, CategoryLevelUpdateSchema
+from crud import category_level as crud
+from sqlalchemy.exc import ProgrammingError
+from auth import user as auth_user
+
+router = APIRouter(prefix="/category_levels", tags=["CategoryLevels"])
+
+
+@router.post("/", response_model=CategoryLevelReadSchema)
+async def create(
+    data: CategoryLevelCreateSchema,
+    payload: dict = Depends(auth_user.get_current_token_payload)
+):
+    username = payload.get("username")
+    password = payload.get("password")
+
+    try:
+        async for session in db_helper.user_pwd_session_getter(username, password):
+            return await crud.create_category_level(session, data)
+    except ProgrammingError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You have no permissions')
+
+
+@router.put("/{category_level_id}", response_model=CategoryLevelReadSchema)
+async def update(
+    category_level_id: int,
+    data: CategoryLevelUpdateSchema,
+    payload: dict = Depends(auth_user.get_current_token_payload)
+):
+    username = payload.get("username")
+    password = payload.get("password")
+
+    try:
+        async for session in db_helper.user_pwd_session_getter(username, password):
+            return await crud.update_category_level(session, category_level_id, data)
+    except ProgrammingError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You have no permissions')
+
+
+@router.get("/", response_model=list[CategoryLevelReadSchema])
+async def get_all(session: AsyncSession = Depends(db_helper.session_getter)):
+    return await crud.get_all_category_levels(session)
+
+
+@router.get("/{category_level_id}", response_model=CategoryLevelReadSchema)
+async def get_by_id(category_level_id: int, session: AsyncSession = Depends(db_helper.session_getter)):
+    category_level = await crud.get_category_level_by_id(session, category_level_id)
+    if not category_level:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Category level with id:{category_level_id} not found"
+        )
+    return category_level
