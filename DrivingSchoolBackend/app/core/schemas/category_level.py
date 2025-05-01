e9d6f4bb1b62e00d35c8f656934a39d93343dde7
maks5s <mskrypnyk0@gmail.com>
2025-05-01 15:02:14 +0300

    Added category level info constraints

--- DrivingSchoolBackend/app/core/schemas/category_level.py
+++ DrivingSchoolBackend/app/core/schemas/category_level.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, Field
 
 
 class CategoryLevelInfoSchema(BaseModel):
-    theory_lessons_count: int = 20
-    practice_lessons_count: int = 20
+    theory_lessons_count: int = Field(20, ge=1, le=100)
+    practice_lessons_count: int = Field(20, ge=1, le=100)
     theory_lessons_duration: time = time(hour=2)
     practice_lessons_duration: time = time(hour=2)
-    minimum_age_to_get: int = 18
+    minimum_age_to_get: int = Field(18, ge=16, le=100)
 
 
 class CategoryLevelBaseSchema(BaseModel):
